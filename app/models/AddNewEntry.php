3dbb75e8fa0cf9468086e5a73ca9064d2996f284
ssv <ssv@ssv-desktop>
2018-03-27 18:48:35 +0300

    ssv.ho.ua v2.0 mvc change addNewEntry remove to model static function checkDate

--- app/models/AddNewEntry.php
+++ app/models/AddNewEntry.php
@@ -13,21 +13,4 @@ class AddNewEntry extends Model {
         $day = date('j');
             
     }
-    
-    public static function checkDate ($date){//Продумать проверку на ТОЛЬКО четное и корректное число месяца
-        $day = $date[0];
-        $month = $date[1];
-        $year = $date[2];
-        if (!($day % 2)){
-                return $arayDate = ['day' => $day, 'month' => $month, 'year' => $year];
-            } else {
-                    if(!($day++ % 2)){
-                        return $arayDate = ['day' => $day, 'month' => $month, 'year' => $year];
-                    } else {
-                        $day+2;
-                        return $arayDate = [$day, $month, $year];
-                    }
-            }
-    }
-
 }
--- core/Model.php
+++ core/Model.php
@@ -5,5 +5,22 @@ class Model extends DB {
     function __construct() {
         parent::__construct();
     }
+    
+    public static function checkDate ($date){//Продумать проверку на ТОЛЬКО четное и корректное число месяца
+        $day = $date[0];
+        $month = $date[1];
+        $year = $date[2];
+        if (!($day % 2)){
+                return $arayDate = ['day' => $day, 'month' => $month, 'year' => $year];
+            } else {
+                    if(!($day++ % 2)){
+                        return $arayDate = ['day' => $day, 'month' => $month, 'year' => $year];
+                    } else {
+                        $day+2;
+                        return $arayDate = [$day, $month, $year];
+                    }
+            }
+    }
+
 
 }
